ac90a6c6e9626968f3ea467fcfcd405160248dbb
OshadhaAnuhas <oshadhanuhas3001@gmail.com>
2025-05-20 20:44:44 +0530

    Create MyFirstProgram.java

---
+++ Practical session 01/MyFirstProgram.java
@@ -0,0 +1,8 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Hi");
+        System.out.print("This is IT1214 Practical Session 1");
+        System.out.println("This prints in a new Line");
+        System.out.println("This is a new Line");
+    }
+}
