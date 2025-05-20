62ce37872f16f12679c57884087646fd9993c363
OshadhaAnuhas <oshadhanuhas3001@gmail.com>
2025-05-20 20:53:17 +0530

    Create P3.java

---
+++ Practical session 01/P3.java
@@ -0,0 +1,6 @@
+public class P3 {
+    public static void main(String[] args) {
+        System.out.println(args[0] + " " + args[1] + " " + args[2]);
+        System.out.println(args[1] + " " + args[0] + " " + args[2]);
+    }
+}
