eb86258ed8b4e4abe52a8d568d1a7c00fb1af101
OshadhaAnuhas <oshadhanuhas3001@gmail.com>
2025-05-26 17:15:39 +0530

    Create App.java

---
+++ Practical session 02/App.java
@@ -0,0 +1,18 @@
+import p1.MyClass;
+import p2.Class2;
+import p2.Class3;
+import p3.Class4;
+
+public class App {
+    public static void main(String[] args) {
+        MyClass obj1 = new MyClass();
+        Class2 obj2 = new Class2();
+        Class3 obj3 = new Class3();
+        Class4 obj4 = new Class4();
+
+        obj1.display();
+        obj2.display();
+        obj3.display();
+        obj4.display();
+    }
+}
